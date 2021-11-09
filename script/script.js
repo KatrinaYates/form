6321a9a2e2717fc722f3224e8de93696cfed9415
Katrina <Krmoore014@gmail.com>
2021-11-08 20:20:37 -0600

    .

--- script/script.js
+++ script/script.js
@@ -1,12 +1,22 @@
 $(document).ready(function() {
-  $("form").submit(function(){
-		if ($('input:checkbox').filter(':checked').length < 1){
-        alert("You forgot to click on what you enjoy!");
-		return false;
-		}
+
+  // validate the email immediatly
+  $("#email").blur (function() {
+    let email = $(this).val();
+    var testEmail = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i;
+    if (!testEmail.test(email))
+      alert("Please enter a valid email address");
   });
 
+  // validate the phone number immediatly
+  $("#phone").blur (function() {
+    let phone = $(this).val();
+    var testPhone = /^((\+[1-9]{1,4}[ \-]*)|(\([0-9]{2,3}\)[ \-]*)|([0-9]{2,4})[ \-]*)*?[0-9]{3,4}?[ \-]*[0-9]{3,4}?$/;
+    if (!testPhone.test(phone))
+      alert("Please enter a valid phone number");
+  });
 
+  // create a flow of choices
   $("#season").change (function() {
     $("#allSeason").removeClass("col-md-12").addClass("col-md-6");
     $("#fall").hide();
@@ -33,11 +43,14 @@ $(document).ready(function() {
     };
 
   });
+
+  // validate that everything has been filled out
+  $("form").submit(function(){
+		if ($('input:checkbox').filter(':checked').length < 1){
+        alert("You forgot to click on what you enjoy!");
+		return false;
+		}
+  });
+
 });
-
-
-// $("inputUserName").blur () {
-//   let userName = $(this).val();
-//   window.alert(username);
-// }
 
